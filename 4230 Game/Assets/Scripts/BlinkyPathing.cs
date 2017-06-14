e8aa6feb5403dffe858b48d20552e86f90e236aa
dcarte51 <dcarte51@uncc.edu>
2017-06-14 16:16:11 -0400

    clyde

--- 4230 Game/Assets/Scripts/BlinkyPathing.cs
+++ 4230 Game/Assets/Scripts/BlinkyPathing.cs
@@ -1,17 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class BlinkyPathing : MonoBehaviour {
     public Transform target;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = target.position;
-	}
+        transform.GetComponent<NavMeshAgent>().destination = target.position;
+
+    }
 }
